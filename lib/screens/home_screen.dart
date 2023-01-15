89c8a8a64ccc925bb29510b1b278f1d20e6f2d6a
Justin <jusin1545@gmail.com>
2023-01-15 09:44:27 +0900

    Flutter FutureBuilder

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,35 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:jootoon/models/webtoon_model.dart';
 import 'package:jootoon/services/api_service.dart';
 
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
+class HomeScreen extends StatelessWidget {
+  /// const -> 컴파일 전에 값을 알 수 있는 타입
+  HomeScreen({super.key});
 
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  List<WebtoonModel> webtoons = [];
-  bool isLoading = true;
-
-  void waitForWebToons() async {
-    webtoons = await ApiService.getTodaysToons();
-    isLoading = false;
-
-    setState(() {});
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    waitForWebToons();
-  }
+  Future<List<WebtoonModel>> webtoons = ApiService.getTodaysToons();
 
   @override
   Widget build(BuildContext context) {
-    print(webtoons);
-    print(isLoading);
-
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
@@ -46,6 +25,18 @@ class _HomeScreenState extends State<HomeScreen> {
             ),
           ),
         ),
+      ),
+
+      /// FutureBuilder -> async/wait(비동기) 처리 대신 사용하는 Widget, Builder -> UI를 그려주는 함수
+      body: FutureBuilder(
+        future: webtoons, // future: await webtoons -> FutureBuilder가 처리
+        /// context -> 부모에게 전달, snapshot -> Future의 상태 파악 가능
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return const Text("There is data!");
+          }
+          return const Text('Loading ...');
+        },
       ),
     );
   }
